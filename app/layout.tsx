8e639b9793a9c2dbff0929c7e2bc839f0e18e71e
Solomon Eseme <kaperskyguru@gmail.com>
2025-12-07 10:31:59 +0100

    fix: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "highlight.js/styles/atom-one-dark.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
 import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 
 export const metadata = {
   title: "Blog - Mastering Backend",
@@ -26,7 +27,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <GoogleTagManager gtmId="G-17J27B98XS" />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-17J27B98XS" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-17J27B98XS');
+        `}
+      </Script>
+
       <body className={cn("min-h-screen bg-background font-sans antialiased")}>
         <ThemeProvider
           attribute="class"
